a03ccb2b5634f02853e7fad3daa847cbfcc505ed
MateuszAdamski <235823@edu.p.lodz.pl>
2023-04-18 18:18:28 +0200

    rename buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,16 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button_simple = findViewById<Button>(R.id.button_simple)
+        val simpleButton = findViewById<Button>(R.id.simpleButton)
 
-        button_simple.setOnClickListener {
-            Toast.makeText(this, "Przycisk został kliknięty", Toast.LENGTH_SHORT).show()
+        simpleButton.setOnClickListener {
             val intent = Intent(this, SimpleCalcActivity::class.java)
             startActivity(intent)
         }
