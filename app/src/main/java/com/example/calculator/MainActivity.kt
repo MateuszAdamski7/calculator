f260e5d01d07bd2367520d07fa6144cebdd6365a
MateuszAdamski <235823@edu.p.lodz.pl>
2023-04-18 18:07:49 +0200

    feat: Landscape view

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.calculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button_simple = findViewById<Button>(R.id.button_simple)
+
+        button_simple.setOnClickListener {
+            Toast.makeText(this, "Przycisk został kliknięty", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SimpleCalcActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
